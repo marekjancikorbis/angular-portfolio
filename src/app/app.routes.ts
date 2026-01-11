4c75e8b2d49a68149ef44cc0d05e468f0cca26da
marekjancikorbis <marekjancik10@gmail.com>
2026-01-11 23:10:45 +0100

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
-
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule]
+})
 export const routes: Routes = [];
